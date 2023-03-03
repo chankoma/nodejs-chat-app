c1c45515d9114f8997b61a4b8bffa4bdec302b8b
chankoma <124317663+chankoma@users.noreply.github.com>
2023-03-03 21:01:00 +0900

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 
 app.post("/login", (req, res) => {
 	for (let i = 1; i < chat_fields + 1; i++) {
-		if (req.body.PASS === process.env.login_pass_ + i) {
+		if (req.body.PASS === process.env.login_pass_1) {
 			res.render("index", {data : req.body.ID});
 		};
 	};
