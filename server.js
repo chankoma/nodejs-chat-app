e48b99bba0a0b81199f03c0933601df62b1098b0
chankoma <124317663+chankoma@users.noreply.github.com>
2023-03-03 23:24:30 +0900

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
 
 app.post("/login", (req, res) => {
 	for (let i = 0; i < target.length; i++) {
-		if (req.body.ID && req.body.PASS === target[i]) {
+		if (req.body.PASS === target[i]) {
 			res.render("index", {data : req.body.ID, fields : i, mess : message[i]});
 		};
 		res.send("miss!");
